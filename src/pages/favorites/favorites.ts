76ec90f7099227c53b04f0d25621904162800548
MahfoudAyoub <60109121+MahfoudAyoub@users.noreply.github.com>
2020-10-23 15:54:29 +0100

    Alerts, Toasts and Loading

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSliding,ToastController,LoadingController, AlertController } from 'ionic-angular';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Dish } from '../../shared/dish';
+
 
 /**
  * Generated class for the FavoritesPage page.
@@ -23,7 +24,10 @@ export class FavoritesPage implements OnInit {
     public navCtrl: NavController,
     public navParams: NavParams,
     private favoriteservice: FavoriteProvider,
-    @Inject("BaseURL") private BaseURL
+    @Inject("BaseURL") private BaseURL,
+    public toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
   ) {}
 
   ngOnInit() {
@@ -39,10 +43,47 @@ export class FavoritesPage implements OnInit {
 
   deleteFavorite(item: ItemSliding, id: number) {
     console.log("delete", id);
-    this.favoriteservice.deleteFavorite(id).subscribe(
-      (favorites) => (this.favorites = favorites),
-      (errmess) => (this.errMess = errmess)
-    );
+
+    let alert = this.alertCtrl.create({
+      title: "Confirm Delete",
+      message: "Do you want to delete Dish " + id,
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Delete cancelled");
+          },
+        },
+        {
+          text: "Delete",
+          handler: () => {
+            let loading = this.loadingCtrl.create({
+              content: "Deleting . . .",
+            });
+            let toast = this.toastCtrl.create({
+              message: "Dish " + id + " deleted successfully",
+              duration: 3000,
+            });
+            loading.present();
+            this.favoriteservice.deleteFavorite(id).subscribe(
+              (favorites) => {
+                this.favorites = favorites;
+                loading.dismiss();
+                toast.present();
+              },
+              (errmess) => {
+                this.errMess = errmess;
+                loading.dismiss();
+              }
+            );
+          },
+        },
+      ],
+    });
+
+    alert.present();
+
     item.close();
   }
 }
